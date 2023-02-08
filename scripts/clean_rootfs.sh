953d25cd0d2489684bc810226bfd3af58f6ee5c1
Han Gao <gaohan@iscas.ac.cn>
2023-02-08 17:08:25 +0800

    fix: upload zst for cirrus
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- scripts/clean_rootfs.sh
+++ scripts/clean_rootfs.sh
@@ -27,7 +27,7 @@ clean_rootfs() {
     export file_name=${DISTURB}-${base_path}-$(date +%Y%m%d%H%M%S)
     mv rootfs.img ${file_name}.img
     zstd -T0 --ultra -20 $file_name.img
-    split -b 800M -d -a 1 $file_name.img.zst $file_name.img.zst.
+    split -b 300M -d -a 1 $file_name.img.zst $file_name.img.zst.
     rm $file_name.img.zst
     ls -al .
 }
